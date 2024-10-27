4ec8b01d78b17584b3424c94d704a3375755ff67
Nividh S <nividh.singh@gmail.com>
2024-10-27 15:33:02 -0400

    More nodes

--- wikipedia.py
+++ wikipedia.py
@@ -13,16 +13,20 @@ def find_links(page):
 
 # Collect all rows to be written to the CSV at once
 rows = [["id1","id2"]]
-pages = ['Discrete mathematics'] + find_links('Discrete mathematics')
-pages = pages[:100]
-
-for page in pages:
+pages = ['Discrete mathematics', 'Portland, Oregon', 'Mars']
+for link in ['Discrete mathematics', 'Portland, Oregon', 'Mars']:
+    pages = pages + find_links(link)
+    
+print("len: " + str(len(pages)))
+for i, page in enumerate(pages):
+    if not i%10:
+        print(i)
     links = find_links(page)
     links = [link for link in links if link in pages]
     for link in links:
         rows.append([page, link])
 
 # Write all rows to the CSV at once
-with open('output_mid.csv', mode='w', newline='', encoding='utf-8') as file:
+with open('output.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerows(rows)
